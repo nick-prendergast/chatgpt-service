6f513b683fa357df609edc6e6c12bcb26ac00c6a
nick prendergast <nick_prendergast@hotmail.com>
2025-02-06 11:18:11 +0100

    working version

--- part_2_testapp/chatgpt-microservice-springboot/src/test/java/com/github/kolomolo/service/openaiclient/restcontroller/ChatGPTJspControllerTest.java
+++ part_2_testapp/chatgpt-microservice-springboot/src/test/java/com/github/kolomolo/service/openaiclient/restcontroller/ChatGPTJspControllerTest.java
@@ -5,7 +5,6 @@ import com.github.kolomolo.service.openaiclient.service.ChatMessageService;
 import com.github.kolomolo.service.openaiclient.service.ChatSessionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
